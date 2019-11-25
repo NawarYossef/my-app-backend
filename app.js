b32e6e90c5db6b84a07b8e011e85cede94f39035
Nawar <nawaryossef2@gmail.com>
2019-11-25 15:34:35 -0500

    fix string quotes issue

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-const express = require('express');
-const path = require('path');
-const generatePassword = require('password-generator');
+const express = require("express");
+const path = require("path");
+const generatePassword = require("password-generator");
 
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.get("/", function (req, res) {
+  res.send(JSON.stringify({ Hello: "World"}));
+ });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
-
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`Password generator listening on ${port}`);
+ app.listen(process.env.PORT || 5000, function () {
+  console.log(`Example app listening on port !${process.env.PORT || 5000}`);
+ });
